96aac69f3f998f665f454b33100605bf2f534f56
cold-flatwhite <canadasun2022@gmail.com>
2024-08-02 11:08:05 -0700

    created three main screen order, provider, search and rendered them in app.js with basic info

--- App.js
+++ App.js
@@ -1,23 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { addDoc, collection } from "firebase/firestore"; 
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import SearchScreen from "./screens/SearchScreen";
+import OrderScreen from "./screens/OrderScreen";
+import ProviderScreen from "./screens/ProviderScreen";
 
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Search" component={SearchScreen} />
+        <Stack.Screen name="Order" component={OrderScreen} />
+        <Stack.Screen name="Provider" component={ProviderScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
