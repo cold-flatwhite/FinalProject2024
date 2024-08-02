66b1d8ea854e3b734be14941209760f99917939e
cold-flatwhite <canadasun2022@gmail.com>
2024-08-02 11:16:29 -0700

    added tabnavigator to show main three screens on the bottom

--- App.js
+++ App.js
@@ -1,22 +1,27 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import SearchScreen from "./screens/SearchScreen";
 import OrderScreen from "./screens/OrderScreen";
 import ProviderScreen from "./screens/ProviderScreen";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabStack = () => {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Search" component={SearchScreen} />
+      <Tab.Screen name="Provider" component={ProviderScreen} />
+      <Tab.Screen name="Order" component={OrderScreen} />
+    </Tab.Navigator>
+  );
+};
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Search" component={SearchScreen} />
-        <Stack.Screen name="Order" component={OrderScreen} />
-        <Stack.Screen name="Provider" component={ProviderScreen} />
-      </Stack.Navigator>
+      <TabStack />
     </NavigationContainer>
   );
 }
-
