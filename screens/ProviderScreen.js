23203c7ab2e343c4b582e6629afc264c4a15f2b7
cold-flatwhite <canadasun2022@gmail.com>
2024-08-02 15:11:40 -0700

    wrapped text and textinput in view for name, address, email, price and create four usestate variables for them.

--- screens/ProviderScreen.js
+++ screens/ProviderScreen.js
@@ -1,10 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, TextInput, Text } from "react-native";
+import React, { useState } from "react";
 
 export default function ProviderScreen() {
+  const [name, setName] = useState("");
+  const [address, setAddress] = useState("");
+  const [email, setEmail] = useState("");
+  const [price, setPrice] = useState("");
   return (
     <View>
-      <Text>ProviderScreen</Text>
+      <View>
+        <Text>Name</Text>
+        <TextInput placeholder="Name" value={name} onChangeText={setName} />
+      </View>
+
+      <View>
+        <Text>Address</Text>
+        <TextInput
+          placeholder="Address"
+          value={address}
+          onChangeText={setAddress}
+        />
+      </View>
+
+      <View>
+        <Text>Email</Text>
+        <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
+      </View>
+
+      <View>
+        <Text>Price</Text>
+        <TextInput placeholder="Price" value={price} onChangeText={setPrice} />
+      </View>
     </View>
-  )
+  );
 }
