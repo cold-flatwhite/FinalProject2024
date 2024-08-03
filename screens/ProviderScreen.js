d9af78bf6d68cd3a71522e40f01407038ea66427
cold-flatwhite <canadasun2022@gmail.com>
2024-08-03 11:25:43 -0700

    added dropdownpicker,  switch function component for experience, breed preference and open for work and change the styles.

--- screens/ProviderScreen.js
+++ screens/ProviderScreen.js
@@ -1,12 +1,21 @@
-import { View, TextInput, Text, StyleSheet, Image } from "react-native";
+import { View, TextInput, Text, StyleSheet, Image,Switch } from "react-native";
 import React, { useState } from "react";
+import DropDownPicker from 'react-native-dropdown-picker';
 
 export default function ProviderScreen() {
   const [name, setName] = useState("");
   const [address, setAddress] = useState("");
   const [email, setEmail] = useState("");
   const [price, setPrice] = useState("");
-
+  const [open, setOpen] = useState(false);
+  const [breedPreference, setBreedPreference] = useState(null);
+  const [experience, setExperience] = useState(false);
+  const [openForWork, setOpenForWork] = useState(false);
+  const [items, setItems] = useState([
+    { label: 'Breed 1', value: 'breed1' },
+    { label: 'Breed 2', value: 'breed2' },
+    
+  ]);
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
@@ -56,7 +65,37 @@ export default function ProviderScreen() {
           onChangeText={setPrice}
         />
       </View>
+      <View style={styles.inputContainer}>
+        <Text>Breed Preference</Text>
+        <DropDownPicker
+          open={open}
+          value={breedPreference}
+          items={items}
+          setOpen={setOpen}
+          setValue={setBreedPreference}
+          setItems={setItems}
+          placeholder="Select breed preference"
+        />
+      </View>
+      <View style={styles.switchContainer}>
+        <Text>Experience</Text>
+        <Switch
+          value={experience}
+          onValueChange={setExperience}
+        />
+      </View>
+
+      <View style={styles.switchContainer}>
+        <Text>Open for work</Text>
+        <Switch
+          value={openForWork}
+          onValueChange={setOpenForWork}
+        />
+      </View>
+
     </View>
+
+
   );
 }
 const styles = StyleSheet.create({
@@ -86,5 +125,12 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     paddingBottom : 10,
+  },
+
+  switchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 16,
   },
 });
