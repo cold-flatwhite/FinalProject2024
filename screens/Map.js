a99ab18fa8bf80301f44d3299b8006874c71c9ab
cold-flatwhite <canadasun2022@gmail.com>
2024-08-09 10:50:21 -0700

    Added Map component to select and confirm a location on the map with a marker and navigation back to Profile screen.

--- screens/Map.js
+++ screens/Map.js
@@ -1,17 +1,50 @@
-import { StyleSheet, Text, View } from "react-native";
-import React from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Reac, { useState } from "react";
+import MapView, { Marker } from "react-native-maps";
 
 const Map = ({ navigation }) => {
   const [location, setLocation] = useState(null);
   return (
     <View>
-      <MapView>
-        
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        onPress={(e) => {
+          setLocation({
+            latitude: e.nativeEvent.coordinate.latitude,
+            longitude: e.nativeEvent.coordinate.longitude,
+          });
+        }}
+      >
+        {location && (
+          <Marker
+            coordinate={{
+              latitude: location.latitude,
+              longitude: location.longitude,
+            }}
+          />
+        )}
       </MapView>
+      <Button
+        title="Confirm selected Location"
+        onPress={() => {
+          navigation.navigate("Profile", { location });
+        }}
+        disabled={!location}
+      />
     </View>
   );
 };
 
 export default Map;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  map: {
+    flex: 1,
+  },
+});
